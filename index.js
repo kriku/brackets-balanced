a45bb0832be24c98a5cb3d72d5f805ce28d0cf1c
Gosha Krikun <g.krikun@innopolis.ru>
2018-07-18 11:16:16 +0300

    Add brackets balance function implementation
    Has chosen stack solution, first that comes to mind

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const brackets = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '<': '>'
+};
+
+const closing = Object.values(brackets);
+
 module.exports = function(str) {
-    return false;
+
+    const stack = [];
+
+    for (let s of str) {
+        if (brackets[s]) {
+            stack.push(brackets[s]);
+        }
+        if (closing.includes(s)) {
+            let bracket = stack.pop();
+            if (bracket != s) {
+                return false;
+            }
+        }
+    }
+
+    return (stack.length == 0);
 };
